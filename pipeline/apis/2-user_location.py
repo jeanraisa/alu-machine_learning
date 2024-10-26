a19f4186b653fe07404f9945e901e7ee103578fb
Raisa <r.irakoze@alustudent.com>
2024-10-26 19:19:09 +0200

    Apis

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,25 +1,21 @@
 #!/usr/bin/env python3
-
-
-""" Return list of ships"""
-
-import requests
+""" Prints the location of a GitHub user specified
+as an arg to the script
+"""
 import sys
-import time
+import requests
+import json
 
+if __name__ == '__main__':
+    url = sys.argv[1]
+    response = requests.get(url)
 
-if __name__ == "__main__":
-    res = requests.get(sys.argv[1])
-
-    if res.status_code == 403:
-        rate_limit = int(res.headers.get('X-Ratelimit-Reset'))
-        current_time = int(time.time())
-        diff = (rate_limit - current_time) // 60
-        print("Reset in {} min".format(diff))
-        # get remaining rate
-
-    elif res.status_code == 404:
-        print("Not found")
-    elif res.status_code == 200:
-        res = res.json()
-        print(res['location'])
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        print(data['location'])
+    elif response.status_code == 403:
+        print('Reset in ')
+    elif response.status_code == 404:
+        print('Not found')
+    else:
+        print('Error:', response.status_code)
